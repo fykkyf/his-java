86135c1a32f6b3d4e590ed1204052968e65c82cb
yty <yty@qq.com>
2023-10-13 14:24:14 +0800

    Initial commit

---
+++ src/main/java/com/woniu/hospital_information_system/HospitalInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.woniu.hospital_information_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalInformationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalInformationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woniu/hospital_information_system/HospitalInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woniu.hospital_information_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalInformationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
