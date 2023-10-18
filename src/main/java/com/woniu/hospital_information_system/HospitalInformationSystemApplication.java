7702c5e2a1ce0bdb2130d555aa6b5be8a2911b70
lpj <lpj@qq.com>
2023-10-18 16:54:05 +0800

    门诊发药sb

--- src/main/java/com/woniu/hospital_information_system/HospitalInformationSystemApplication.java
+++ src/main/java/com/woniu/hospital_information_system/HospitalInformationSystemApplication.java
@@ -12,7 +12,7 @@ public class HospitalInformationSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HospitalInformationSystemApplication.class, args);
-        System.out.println("我是六哥");
+
     }
 
 }
